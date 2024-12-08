f203142a181a0b71a392785c009ea6655f70b6d5
MrXiyi666 <MrXiyi@outlook.com>
2024-12-08 08:39:32 +0800

    2024_12_8_8_39

--- phpfile/Read_Square_Data_List.php
+++ phpfile/Read_Square_Data_List.php
@@ -5,17 +5,11 @@ if(!is_dir('./Square_Data')){
 }
 $_array = scandir('./Square_Data', 1);
 $_return = array();
-$index = 0;
 foreach ($_array as $value){
-    if($index >= 100){
-        break;
-    }
     if($value=='.' || $value=='..'){
         continue;
-    }else{
-        $_return[] = $value;
-        $index++;
     }
+    $_return[] = $value;
 }
 if(count($_return) == 0){
     die("no");
