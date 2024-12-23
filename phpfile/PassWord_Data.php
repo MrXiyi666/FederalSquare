8f372e641a53441773559186584ddc85440ccfa4
王子亮 <MrXiyi@outlook.com>
2024-12-24 06:58:21 +0800

    修改变量名

--- phpfile/PassWord_Data.php
+++ phpfile/PassWord_Data.php
@@ -1,14 +1,14 @@
 <?php
-    $Read_PassWord = $_POST['Read_PassWord'];
+    $PassWord = $_POST['PassWord'];
 
     $File_Name = "Access_PassWord.txt";
     if(is_file($File_Name)){
         $PassWord = file_get_contents($File_Name);
         if(strlen($PassWord) > 0){
-            if(empty($Read_PassWord)){
+            if(empty($PassWord)){
                 die("Null_PassWord");
             }
-            if($Read_PassWord != $PassWord){
+            if($PassWord != $PassWord){
                 die("Error_PassWord");
             }
         }
