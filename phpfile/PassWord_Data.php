b09cd8be0658f723b48531198681cf62b3344e65
王子亮 <MrXiyi@outlook.com>
2024-12-24 07:27:44 +0800

    修复bug

--- phpfile/PassWord_Data.php
+++ phpfile/PassWord_Data.php
@@ -1,14 +1,14 @@
 <?php
-    $PassWord = $_POST['PassWord'];
+    $Read_PassWord = $_POST['PassWord'];
 
     $File_Name = "Access_PassWord.txt";
     if(is_file($File_Name)){
         $PassWord = file_get_contents($File_Name);
         if(strlen($PassWord) > 0){
-            if(empty($PassWord)){
+            if(empty($Read_PassWord)){
                 die("Null_PassWord");
             }
-            if($PassWord != $PassWord){
+            if($Read_PassWord != $PassWord){
                 die("Error_PassWord");
             }
         }
