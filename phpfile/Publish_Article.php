5dbdc8bb6ae6faf0b3770974d7b292a7b3a6857a
王子亮 <MrXiyi@outlook.com>
2024-12-24 07:33:07 +0800

    修改了逻辑

--- phpfile/Publish_Article.php
+++ phpfile/Publish_Article.php
@@ -1,21 +1,19 @@
 <?php
 
     include 'PassWord_Data.php';
-
     $account = $_POST['account'];
     $name = $_POST['name'];
     $data = $_POST['data'];
 
-
+    if(!fopen("./Account/$account/Data/$name" . ".json", "w")){
+		die ("no_account");	
+	}
+	
 	$file = fopen("./Square_Data/$name" . ".json", "w");
 	if(!$file){
 		die ("no_network");
 	}
 	
-	if(!fopen("./Account/$account/Data/$name" . ".json", "w")){
-		die ("no_account");	
-	}
-
 	echo "ok";
     fwrite($file, $data);
     fclose($file);
