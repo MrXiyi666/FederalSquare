36709f67619ce02c91c6757bd1b0e0438d5691a5
王子亮 <MrXiyi@outlook.com>
2024-12-29 06:19:26 +0800

    优化了逻辑

--- phpfile/PassWord_Data.php
+++ phpfile/PassWord_Data.php
@@ -4,13 +4,12 @@
     $File_Name = "Access_PassWord.txt";
     if(is_file($File_Name)){
         $PassWord = file_get_contents($File_Name);
-        if(strlen($PassWord) > 0){
-            if(empty($Read_PassWord)){
+		if($PassWord and strlen($PassWord) > 0){
+		   if(empty($Read_PassWord)){
                 die("Null_PassWord");
             }
             if($Read_PassWord != $PassWord){
                 die("Error_PassWord");
             }
-        }
+		}
     }
-
