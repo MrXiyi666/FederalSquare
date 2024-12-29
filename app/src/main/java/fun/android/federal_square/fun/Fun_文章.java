f032b7167d2e92a6ce2255eeea0eb350194e693e
王子亮 <MrXiyi@outlook.com>
2024-12-29 09:14:37 +0800

    修复bug

--- app/src/main/java/fun/android/federal_square/fun/Fun_文章.java
+++ app/src/main/java/fun/android/federal_square/fun/Fun_文章.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
