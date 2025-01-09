ca2c438ce0f966632b8f764b6c14d825f255d176
王子亮 <MrXiyi@outlook.com>
2025-01-09 08:21:18 +0800

    修复了bug

--- phpfile/Fun.php
+++ phpfile/Fun.php
@@ -1,12 +1,8 @@
 <?php
 
 function remove_array($jianzhi, $shuju){
-    $fujian = array();
-    foreach($jianzhi as $value){
-        if($shuju == $value){
-            continue;
-        }
-		$fujian[] = $value;
-    }
-    return $fujian;
+    return array_filter($jianzhi, 
+    function($value) use ($shuju) {
+        return $value !== $shuju;
+    });
 }
