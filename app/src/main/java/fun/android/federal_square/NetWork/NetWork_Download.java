4ed08d7da649b6daaff90e8c50dcbf6d1a898142
王子亮 <MrXiyi@outlook.com>
2025-11-23 11:05:16 +0800

    refactor: Rename NetWork_Main to NetWork_Download and refactor logic

--- app/src/main/java/fun/android/federal_square/NetWork/NetWork_Download.java
+++ app/src/main/java/fun/android/federal_square/NetWork/NetWork_Download.java
@@ -10,71 +10,58 @@ import fun.android.federal_square.system.Fun;
 import fun.android.federal_square.system.Fun_文件;
 import fun.android.federal_square.system.Static;
 import fun.android.federal_square.window.Window_Progress_Bar;
-import fun.android.federal_square.window.Window_View_Image;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
-public class NetWork_Main {
+public class NetWork_Download {
     private final Activity activity;
     private final String class_name;
     private String file_name;
-    private Window_Progress_Bar window_progress_bar;
-    public NetWork_Main(Activity activity){
+    private final Window_Progress_Bar window_progress_bar;
+    public NetWork_Download(Activity activity){
         this.activity = activity;
         this.class_name = this.getClass().getSimpleName();
-    }
-
-    public void 事件(String string){
-
-    }
-
-    public void create(){
         window_progress_bar = new Window_Progress_Bar(activity);
     }
+    /**
+     * 传递文件名 下载完毕对文件进行操作
+     * @param name 文件名字
+     */
+    public void 事件(String name){
+
+    }
+
     /**
      * 刷新一些内容内容
      * @param progress 下载进度
      */
-    public void update(int progress){
-        activity.runOnUiThread(()->{
-            window_progress_bar.setProgress(progress);
-        });
+    private void update(int progress){
+        window_progress_bar.setProgress(progress);
     }
 
-    public void start(String url){
-        Log.w(class_name, "线程开启");
+
+
+    public void Start(String url){
         Request request = new Request.Builder().url(url).build();
 
         file_name = Fun.getFileNameFromUrl(url);
-        create();
+
         if (file_name == null || file_name.isEmpty()) {
-            Log.w(class_name, "未找到文件名 下载取消");
+
             return;
         }
         Static.okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if(!response.isSuccessful()){
-                    if (response.body() != null) {
-                        response.body().close();
-                    }
-                    Log.w(class_name, "下载失败1");
-                    activity.runOnUiThread(()->{
-                        window_progress_bar.close();
-                    });
+                    response.body().close();
+                    window_progress_bar.close();
                     return;
                 }
                 ResponseBody body = response.body();
-                if (body == null) {
-                    Log.w(class_name, "下载失败2");
-                    activity.runOnUiThread(()->{
-                        window_progress_bar.close();
-                    });
-                    return;
-                }
                 写入文件(body);
 
             }
@@ -82,15 +69,13 @@ public class NetWork_Main {
             public void onFailure(Call call, IOException e) {
                 final String errorMsg = "请求出错: " + e.getMessage();
                 Log.w(class_name, errorMsg);
-                activity.runOnUiThread(()->{
-                    window_progress_bar.close();
-                });
+                window_progress_bar.close();
+                事件("");
             }
         });
     }
 
     private void 写入文件(ResponseBody body) throws IOException {
-        // 6. 流写入文件（核心逻辑）
         Log.w(class_name, "保存文件");
         InputStream in = body.byteStream();
 
@@ -119,8 +104,9 @@ public class NetWork_Main {
         out.close();
         in.close();
         body.close();
-        this.activity.runOnUiThread(()->{
+        activity.runOnUiThread(()->{
             window_progress_bar.close();
+            事件("");
         });
     }
 
